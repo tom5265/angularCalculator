a1ccdf637a20a8b5e1d374472b726b33c78f314e
Tom Moore <tom5265@gmail.com>
2016-03-29 22:12:18 -0500

    two different ways of error handler

--- client/js/controller.js
+++ client/js/controller.js
@@ -2,20 +2,46 @@ var controllers = angular.module('MyApp.controller', []);
 
 controllers.controller('MainController', ['$scope', function ($scope) {
     $scope.adding = function () {
+       if ($scope.operandOne == null || $scope.operandTwo == null) {
+            alert('Both operands are required!');
+            return
+        }
         console.log('debris');
-        $scope.result = $scope.operandOne + $scope.operandTwo; 
+        $scope.answer = $scope.operandOne + $scope.operandTwo;
+        $scope.result = 'The result of adding ' + $scope.operandOne + ' and ' + $scope.operandTwo + ' is ' + $scope.answer;
+        $scope.clearForms(); 
     }
     $scope.subtracting = function () {
+        $scope.errorCatch();
         console.log('hello');
-        $scope.result = $scope.operandOne - $scope.operandTwo;
+        $scope.answer = $scope.operandOne - $scope.operandTwo;
+        $scope.result = 'The result of subtracting ' + $scope.operandOne + ' and ' + $scope.operandTwo + ' is ' + $scope.answer;
+        $scope.clearForms();
     }
     $scope.multiplying = function () {
+        $scope.errorCatch();
         console.log('I multiplied!');
-        $scope.result = $scope.operandOne * $scope.operandTwo;
+        $scope.answer = $scope.operandOne * $scope.operandTwo;
+        $scope.result = 'The result of multiplying ' + $scope.operandOne + ' and ' + $scope.operandTwo + ' is ' + $scope.answer;
+        $scope.clearForms();
     }
     $scope.dividing = function () {
         console.log('I divided!');
-        $scope.result = $scope.operandOne / $scope.operandTwo;
+        $scope.answer = $scope.operandOne / $scope.operandTwo;
+        $scope.result = 'The result of dividing ' + $scope.operandOne + ' and ' + $scope.operandTwo + ' is ' + $scope.answer;
+        $scope.clearForms();
+    }
+    
+    $scope.clearForms = function () {
+        $scope.operandOne = 0;
+        $scope.operandTwo = 0;
+    }
+    
+    $scope.errorCatch = function () {
+        if ($scope.operandOne == null || $scope.operandTwo == null) {
+            alert('Both operands are required!');
+            return
+        }
     }
     
 }])
