32db9bd815016301a21b6f7f5d0cf841e6ec7e8b
Tom Moore <tom5265@gmail.com>
2016-03-29 21:34:02 -0500

    calculator working

--- client/js/controller.js
+++ client/js/controller.js
@@ -3,6 +3,19 @@ var controllers = angular.module('MyApp.controller', []);
 controllers.controller('MainController', ['$scope', function ($scope) {
     $scope.adding = function () {
         console.log('debris');
+        $scope.result = $scope.operandOne + $scope.operandTwo; 
+    }
+    $scope.subtracting = function () {
+        console.log('hello');
+        $scope.result = $scope.operandOne - $scope.operandTwo;
+    }
+    $scope.multiplying = function () {
+        console.log('I multiplied!');
+        $scope.result = $scope.operandOne * $scope.operandTwo;
+    }
+    $scope.dividing = function () {
+        console.log('I divided!');
+        $scope.result = $scope.operandOne / $scope.operandTwo;
     }
     
 }])
